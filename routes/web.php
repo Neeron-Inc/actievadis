72c4c9ed4eefd1768e72031d0ffaa9b5cbcca632
D-Jordy <kaaljordy@gmail.com>
2023-09-12 10:57:48 +0200

    Fix (routing): Use middleware

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,28 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ActivityController;
 
 Route::get('/', function () {
-  return view('welcome');
+    return view('welcome');
 });
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-])->group(function () {
-  Route::get('/dashboard', function () {
-    return view('dashboard');
-  })->name('dashboard');
-});
+])->group(fn() => authRoutes());
 
-Route::controller(ActivityController::class)->group(function () {
-  Route::get('/activities', 'index')->name('activity.overview');
-  Route::get('/activity/{id}', 'show')->name('activity.show');
-  Route::post('/activity/{activity}/register', 'register')->name('activity.register');
-});
+function authRoutes()
+{
+    Route::get('/dashboard', fn() => redirect()->route('activity.overview'))
+        ->name('dashboard');
 
-Route::controller(AdminController::class)->group(function () {
-  Route::get('/admin', 'index')->name('admin.overview');
-  Route::put('/admin/toggle/{user}', 'update')->name('admin.toggle');
-});
+    Route::controller(ActivityController::class)->group(function () {
+        Route::get('/activities', 'index')->name('activity.overview');
+        Route::get('/activity/{id}', 'show')->name('activity.show');
+        Route::post('/activity/{activity}/register', 'register')->name('activity.register');
+    });
+
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/admin', 'index')->name('admin.overview');
+        Route::put('/admin/toggle/{user}', 'update')->name('admin.toggle');
+    });
+}
