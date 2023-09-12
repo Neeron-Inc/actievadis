d655d4f043a7112592147d91c6b6a7cd2a514383
TiesBerendsen <ties.berendsen0205@gmail.com>
2023-09-12 11:45:59 +0200

    overzicht front-end

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,14 @@
+
+<a href="" class="w-[350px] bg-white">
+    <div class="w-full h-[350px] bg-center flex justify-center items-center group overflow-hidden" style="background-image: url(https://images.pexels.com/photos/2158963/pexels-photo-2158963.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)">
+        <div class="w-full h-full flex justify-center items-center bg-gray-950 bg-opacity-0 group-hover:bg-opacity-40 ease-in-out transition-all">
+            <div class="w-2/6 h-2/6 hidden group-hover:flex rounded-full bg-[#F5AF00] justify-center items-center"> 
+                <img src="{{ asset("img/pijl.svg") }}" alt="">                              
+        </div>
+        </div>
+    </div>
+        <div class="text-center py-4 px-8 bg-white">
+            <h6 class="text-[#0F132F] mb-1">Lorem Ipsum</h6>
+            <p class="text-xs text-[#717696]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+        </div>
+</a>
