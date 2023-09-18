ee6ae0bec10aec59170d142a2534e1667c8d5969
Xanxius <kenai.freriks@student.graafschapcollege.nl>
2023-09-18 15:36:52 +0200

    frontend Activity-show AdminButton DeelnemerCount

--- resources/views/activity/show.blade.php
+++ resources/views/activity/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <aside class="z-0 mt-24 mb-24 pb-28 bg-[#0f132f] w-8/12 h-auto">
         <article class="ml-96 text-xl">
-            <img class="h-[600px] w-[600px] ml-[650px] mt-24 absolute" src="{{$activity->image}}" alt="Hey cassandra">
+            <img class="h-[600px] w-[600px] ml-[650px] mt-24 absolute" src="{{$activity->image}}" alt="Hier komt een image">
             <p class="pt-44 text-[#f5af00]">Samen met je collega's!</p>
             <h2 class="text-6xl text-white pb-4">{{$activity->name}}</h2>
             <div class="text-white w-8/12 mb-16 flex flex-col">
@@ -12,7 +12,7 @@
                 <p class="mb-2">Prijs: {{$activity->price}}</p>
                 <p class="mb-2">Begintijd: {{$activity->start_date}}</p>
                 <p class="mb-2">Eindtijd: {{$activity->end_date}}</p>
-                <p class="mb-2">deelnemers: {{$activity->min_participants}} tot {{$activity->max_participants}}</p>
+                <p class="mb-2">deelnemers: {{$activity->users->count()}}</p>
                 @isset($activity->needs)
                     <p class="mb-2">Benodigdheden: {{$activity->needs}}</p>
                 @endisset
@@ -23,11 +23,13 @@
                     @csrf
                     <x-button type="submit">Inschijven</x-button>
                 </form>
-                <form action="{{ route('activity.delete', $activity) }}" method="POST">
-                    @csrf
-                    @method('delete')
-                    <x-delete-button></x-delete-button>
-                </form>
+                @if(auth()->user()->is_admin)
+                    <form action="{{ route('activity.delete', $activity) }}" method="POST">
+                        @csrf
+                        @method('delete')
+                        <x-delete-button></x-delete-button>
+                    </form>
+                @endif
             </div>
         </article>
     </aside>
