5b7f98fec6faa884dc6ae03079c038530bc1fb83
Jordy <91130457+D-Jordy@users.noreply.github.com>
2023-09-26 10:51:40 +0200

    Implement sugestion

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Activity;
-use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
