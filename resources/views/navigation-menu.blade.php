8ae592254f565e4f1076d9f75da111fa4e0a56cf
jbe05 <jonasbeernink@gmail.com>
2023-10-02 13:33:06 +0200

    skill issue fix

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,15 +2,6 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between py-3">
-<<<<<<< Updated upstream
-            <!-- Home svg with text -->
-            <a href="{{route('activity.overview')}}" class="flex items-center gap-4">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                </svg>
-                <h1 class="text-[#0F132F] font-bold text-xl">Home</h1>
-            </a>
-=======
             <div class="flex gap-6 items-center">
                 <a href="{{route('activity.overview')}}" class="flex items-center gap-3 text-gray-500 hover:text-gray-700 ">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
@@ -25,7 +16,6 @@
                     <h1 class="font-semibold text-lg">Admin</h1>
                 </a>
             </div>
->>>>>>> Stashed changes
 
 
             <div class="hidden space-x-8 sm:flex">
