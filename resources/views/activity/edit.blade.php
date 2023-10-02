984a61b788a1aaf81853e018962b09b4f6986a3a
jbe05 <jonasbeernink@gmail.com>
2023-10-02 11:28:45 +0200

    navbar button

--- resources/views/activity/edit.blade.php
+++ resources/views/activity/edit.blade.php
@@ -8,6 +8,8 @@
 
             <!-- Insert image here -->
             <div class="w-1/2 flex flex-col items-center justify-center bg-no-repeat bg-cover"  style="background-image: url({{ file_exists(public_path('storage/' . $activity->image)) ? asset("storage/" . $activity->image) : $activity->image }})">
+                <!-- in topleft use x-exit.bladephp -->
+
                 <svg data-darkreader-inline-stroke="" aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-1/5 text-[#0F132F]">
                     <path d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" stroke-linecap="round" stroke-linejoin="round"></path>
                 </svg>
