09c5fad8d83de2985e338be82b73b106d69539a1
jbe05 <jonasbeernink@gmail.com>
2023-10-02 13:11:07 +0200

    comments

--- resources/views/activity/edit.blade.php
+++ resources/views/activity/edit.blade.php
@@ -4,12 +4,9 @@
     <div class="flex w-full bg-[#e2e8f0]">
         <form class="flex w-full gap-4" action="{{ route('activity.update', $activity->id) }}" method="post" enctype="multipart/form-data">
             @csrf
-            @method('PUT') <!-- Assuming you are using the PUT method for updates -->
+            @method('PUT')
 
-            <!-- Insert image here -->
             <div class="w-1/2 flex flex-col items-center justify-center bg-no-repeat bg-cover"  style="background-image: url({{ file_exists(public_path('storage/' . $activity->image)) ? asset("storage/" . $activity->image) : $activity->image }})">
-                <!-- in topleft use x-exit.bladephp -->
-
                 <svg data-darkreader-inline-stroke="" aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-1/5 text-[#0F132F]">
                     <path d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" stroke-linecap="round" stroke-linejoin="round"></path>
                 </svg>
@@ -20,7 +17,6 @@
             </div>
 
             <div class="w-1/2 grid grid-cols-2 gap-4 my-12">
-                <!-- activiteit aanmaken -->
                 <h1 class="text-[#0F132F] text-5xl col-span-2">Activiteit aanpassen</h1>
 
                 <div class="form-group">
