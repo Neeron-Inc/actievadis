41a9d765462c695cb5e1e7541adacf4e989d1128
TiesBerendsen <ties.berendsen0205@gmail.com>
2023-10-03 11:49:25 +0200

    aspect fixes
    back button, aspect fixes, kleine reworks

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,12 @@
+<<<<<<< Updated upstream
 <a href="{{ route('activity.show', $activity) }}" class="w-[350px] bg-white transition-all ease-in-out" id="card" name="card">
     <div class="w-full h-[350px] bg-center flex justify-center items-center group overflow-hidden" style="background-image: url({{ file_exists(public_path('storage/' . $activity->image)) ? asset("storage/" . $activity->image) : $activity->image }})">
+=======
+<a href="{{ route('activity.show', $activity) }}" class="w-[20vw] bg-white transition-all ease-in-out shadow-lg" id="card" name="card">
+    <div class="w-full h-[40vh] bg-center flex justify-center items-center group overflow-hidden bg-cover" style="background-image: url({{ file_exists(public_path('storage/' . $activity->image)) ? asset("storage/" . $activity->image) : $activity->image }})">
+>>>>>>> Stashed changes
         <div class="w-full h-full flex justify-center items-center bg-gray-950 bg-opacity-0 group-hover:bg-opacity-40 ease-in-out transition-all">
-            <div class="w-2/6 h-2/6 hidden group-hover:flex rounded-full bg-[#F5AF00] justify-center items-center">
+            <div class="w-[7vw] h-[7vw] hidden group-hover:flex rounded-full bg-[#F5AF00] justify-center items-center">
                 <img src="{{ asset("img/pijl.svg") }}" alt="">
             </div>
         </div>
