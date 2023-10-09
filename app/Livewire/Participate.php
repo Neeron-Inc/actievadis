8f3e978c4f381902146b7bb74d2770242ef3df0c
Xanxius <kenai.freriks@student.graafschapcollege.nl>
2023-10-09 16:00:30 +0200

    Refresh on Participate and Resign

--- app/Livewire/Participate.php
+++ app/Livewire/Participate.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Activity;
 use Illuminate\View\View;
+use Livewire\Features\SupportRedirects\Redirector;
 use Livewire\Component;
 
 class Participate extends Component
@@ -13,11 +14,12 @@ class Participate extends Component
     public Activity $activity;
     public string $comment = '';
 
-    public function participate(): void
+    public function participate(): Redirector
     {
         auth()->user()->participate($this->activity, $this->comment);
         $this->toggleShow();
         $this->participates = true;
+        return redirect(request()->header('Referer'));
     }
 
     public function toggleShow(): void
@@ -29,10 +31,11 @@ class Participate extends Component
         }
     }
 
-    public function resign(): void
+    public function resign(): Redirector
     {
         auth()->user()->resign($this->activity);
         $this->participates = false;
+        return redirect(request()->header('Referer'));
     }
 
     public function render(): View
