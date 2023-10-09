d4a9aff21053641eeccbc880bc685a7a397bc680
jbe05 <jonasbeernink@gmail.com>
2023-10-09 11:01:23 +0200

    admin only

--- resources/views/livewire/participant-comments.blade.php
+++ resources/views/livewire/participant-comments.blade.php
@@ -1,27 +1,28 @@
 <main class="z-50">
-    <p wire:click="toggleShow" class="mb-2 flex items-center"><span class="text-[#f5af00] pr-1"><x-group-icon></x-group-icon></span> {{$participants->count()}}
+    <p @if(auth()->user()->is_admin) wire:click="toggleShow" @endif class="@if(auth()->user()->is_admin) cursor-pointer @endif mb-2 flex items-center"><span class="text-[#f5af00] pr-1"><x-group-icon></x-group-icon></span> {{$participants->count()}}
     </p>
+    @if(auth()->user()->is_admin)
+        <section wire:click.away="toggleShow"
+                 class="@if (!$showModal) hidden @endif fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
 
-    <section wire:click.away="toggleShow"
-             class="@if (!$showModal) hidden @endif fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <content class="bg-white rounded-lg w-1/2 p-4 pt-2">
+                <header class="flex justify-between items-center">
+                    <h2 class="text-2xl font-bold text-black mb-2">Alle comments</h2>
 
-        <content class="bg-white rounded-lg w-1/2 p-4 pt-2">
-            <header class="flex justify-between items-center">
-                <h2 class="text-2xl font-bold text-black mb-2">Alle comments</h2>
+                    <p class="text-black cursor-pointer" wire:click="toggleShow">
+                        <x-exit wire:click="toggleShow"/>
+                    </p>
+                </header>
 
-                <p class="text-black cursor-pointer" wire:click="toggleShow">
-                    <x-exit wire:click="toggleShow"/>
-                </p>
-            </header>
-
-            <div class="flex flex-col gap-6">
-                @foreach($participants as $participant)
-                    <div class="flex gap-2 text-black">
-                        <p class="text-xl font-bold whitespace-nowrap">{{ $participant->user->name }}</p>
-                        <p>{{$participant->comment}}</p>
-                    </div>
-                @endforeach
-            </div>
-        </content>
-    </section>
+                <div class="flex flex-col gap-6">
+                    @foreach($participants as $participant)
+                        <div class="flex gap-2 text-black">
+                            <p class="text-xl font-bold whitespace-nowrap">{{ $participant->user->name }}</p>
+                            <p>{{$participant->comment}}</p>
+                        </div>
+                    @endforeach
+                </div>
+            </content>
+        </section>
+    @endif
 </main>
